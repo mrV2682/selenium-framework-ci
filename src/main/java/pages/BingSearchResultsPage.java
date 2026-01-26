f2d0b58cea7fe83be832e3574d69953bd48a8366
Vuong Tran <thevuong268@gmail.com>
2026-01-26 14:38:56 +0700

    corect issue on staging env 4th time

--- src/main/java/pages/BingSearchResultsPage.java
+++ src/main/java/pages/BingSearchResultsPage.java
@@ -6,12 +6,19 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 public class BingSearchResultsPage extends BasePage {
 
     private final By resultsContainer = By.id("b_results");
-    private final By results = By.cssSelector("#b_results > li");
+    private final By results = By.cssSelector("#b_results li.b_algo");
 
     public int getResultCount() {
-        wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.cssSelector("li.b_algo")
-        ));
-        return findAll(By.cssSelector("li.b_algo")).size();
+
+        // 1. Chờ container kết quả hiển thị
+        wait.until(ExpectedConditions.visibilityOfElementLocated(resultsContainer));
+
+        // 2. Chờ CÓ ÍT NHẤT 1 KẾT QUẢ (business condition)
+        wait.until(driver ->
+                driver.findElements(results).size() > 0
+        );
+
+        // 3. Đếm kết quả
+        return findAll(results).size();
     }
 }
