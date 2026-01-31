b70171e01119d1a12793479fccdf9c17a5b917ae
Vuong Tran <thevuong268@gmail.com>
2026-01-31 11:39:38 +0700

    fix retry issue - do not retry for API test

--- src/test/java/retry/RetryAnalyzer.java
+++ src/test/java/retry/RetryAnalyzer.java
@@ -6,10 +6,25 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int retryCount = 0;
-    private static final int MAX_RETRY = 2; // retry tối đa 2 lần
+    private static final int MAX_RETRY = 2; // retry 2 times
 
     @Override
     public boolean retry(ITestResult result) {
+
+        /**
+         * Do not retry for API test
+         */
+        String[] groups = result.getMethod().getGroups();
+        if (groups != null){
+            for (String group : groups){
+                if ("api".equalsIgnoreCase(group)){
+                    return false;
+                }
+            }
+        }
+        /**
+         * Retry onlu for NON-API test
+         */
         if (retryCount < MAX_RETRY) {
             retryCount++;
             System.out.println(
